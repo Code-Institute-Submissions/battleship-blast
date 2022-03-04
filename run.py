d590b2e62b0dcbaa15c64530907e79f577c263f4
Stephie <stephaniecrocker91@gmail.com>
2022-03-04 19:57:31 +0000

    Pip install termcolor and create launch_intro function to welcome user to game

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
+from termcolor import colored
+
 import random
+
+def launch_intro():
+    """
+    Initial function that loads Intro 
+    and requests players name
+    """
+    print(colored("WELCOME TO BATTLESHIPS BLAST \n",'green'))
+
 
 def generate_random_coordinates():
     """
@@ -9,10 +19,12 @@ def generate_random_coordinates():
     random_coordinates = []
     while len(random_coordinates) < 5:
         x = random.randint(0,4)
-        y = random.randint (0,4)
+        y = random.randint(0,4)
         coordinate = x,y
         if coordinate not in random_coordinates:
             random_coordinates.append(tuple(coordinate))
     return random_coordinates
     
-generate_random_coordinates()()
+
+launch_intro()
+generate_random_coordinates()
