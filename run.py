8991c75ef99c529ca3bf7dcbbb5121e0c9cdab40
Stephie <stephaniecrocker91@gmail.com>
2022-03-04 19:29:35 +0000

    Initial commit coding function generate_randome_coordinates that will generate 5 random coordinates where battleships will be placed on board

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def generate_random_coordinates():
+    """
+    Function that generated 5 random unique 
+    coordinates where battleships will
+    be hidden on the board.
+    """
+    random_coordinates = []
+    while len(random_coordinates) < 5:
+        x = random.randint(0,4)
+        y = random.randint (0,4)
+        coordinate = x,y
+        if coordinate not in random_coordinates:
+            random_coordinates.append(tuple(coordinate))
+    print(random_coordinates)
+    
+generate_random_coordinates()()
