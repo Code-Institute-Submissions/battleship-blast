a14097d8fb5ceb8a49d4b8f464cad3d1acca9311
Stephie <stephaniecrocker91@gmail.com>
2022-03-05 20:17:12 +0000

    Add code for validating player input in menu to accept only 1, 2, 3

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from termcolor import colored
-from pyfiglet import Figlet
-
 import random
+
+from pyfiglet import Figlet
+from termcolor import colored
 
 def launch_intro():
     """
@@ -21,20 +21,52 @@ def game_menu():
     """
     print(colored('************************************************ \n','green'))
     print(colored("G A M E    M E N U \n",'green'))
+    print("\n")
     print(colored("War is imminent....",'green'))
     print(colored(f'{player_name}, prepare for battle! \n','green'))
     print(colored("Game Instructions--> 1",'green'))
     print(colored("Launch game--> 2",'green'))
     print(colored("Exit game--> 3",'green'))
     global menu_selection
-    menu_selection=int(input())
+    menu_selection=input()
+    menu_key_options(menu_selection)
+    validate_key(menu_selection)
+
+def validate_key(data):
+    """
+    Function that validates input data and raises errors accordingly.
+    Correct data should be either 1, 2 or 3
+    """
+    try:
+        keys="1 2 3"
+        if data not in keys:
+            raise ValueError(
+                f"Only input 1, 2, 3 are valid. You provided invalid data {data}."
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}")
 
 
 def game_instructions():
     """
     Game instructions
     """
-    print('Game instructions')
+    print(colored('************************************************ \n','green'))
+    print(colored("G A M E    I N S T R U C T I O N S \n",'green'))
+    print("\n")
+    print(colored("• The battlefield is displayed in a 5 x 5 grid.",'green'))
+    print(colored("• In this field there are 5 battleships hidden.",'green'))
+    print(colored("• Each battleship takes up one single coordinate. For example: (0, 1) \n",'green'))
+    print(colored("• Enter the coordinates to launch the missil. \n",'green'))
+    print(colored("• You have a total of 15 missils.",'green'))
+    print(colored("• You must sink ALL battleships to defeat the enemy. \n",'green'))
+    print(colored("• The top left corner is coordinate. (0, 0)",'green'))
+    print(colored("• The bottom right corner is coordinates.\n (4, 4)",'green'))
+    print("\n")
+    print(colored("Go back to menu--> M",'green'))
+    menu_selection= input().upper() 
+    validate_key(menu_selection)
+    menu_key_options(menu_selection)  
 
 def launch_game():
     """
@@ -48,23 +80,19 @@ def exit_game():
     """
     print('Exit Game')
 
-
 def menu_key_options(key_selection):
     """
     Function that directs the user to their chosen 
     option
     """
-    if key_selection == 1:
+    if key_selection == "1":
         game_instructions()
-    elif key_selection == 2:
+    elif key_selection == "2":
         launch_game()
-    elif key_selection == 3:
+    elif key_selection == "3":
         exit_game()
-    else: 
-        print('Invalid entry')
-    
-   
-
+    elif key_selection == "M":
+        game_menu()
 
 def generate_random_coordinates():
     """
@@ -82,11 +110,8 @@ def generate_random_coordinates():
             random_coordinates.append(tuple(coordinate))
     return random_coordinates
 
-
-
-
 launch_intro()
 game_menu()
-menu_key_options(menu_selection)
-generate_random_coordinates()
-print(random_coordinates)
+
+#generate_random_coordinates()
+#print(random_coordinates)
